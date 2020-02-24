dfe2e41f7ab78a6bb9d1f4e4626c1d272ec778cf
tjcj211 <timcarta@hotmail.com>
2020-02-24 11:32:13 -0500

    Finished demo8

---
+++ app/src/main/java/edu/quinnipiac/ser210/bitsandpizzas/MainActivity.java
@@ -0,0 +1,42 @@
+package edu.quinnipiac.ser210.bitsandpizzas;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
+
+import androidx.core.view.MenuItemCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ShareActionProvider shareActionProvider;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //inflate the menu; this adds items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        shareActionProvider =
+                (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+        setShareActionIntent("Want to join me for pizza?");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void setShareActionIntent(String text) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, text);
+        shareActionProvider.setShareIntent(intent);
+    }
+}
---
+++ app/src/main/java/edu/quinnipiac/ser210/bitsandpizzas/OrderActivity.java
@@ -0,0 +1,22 @@
+package edu.quinnipiac.ser210.bitsandpizzas;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplatHomeAsUpEnabled(true);
+    }
+}
